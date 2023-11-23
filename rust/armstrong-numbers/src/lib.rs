d9431aeabfc0fe639c4b1c799d22d20e70d2732d
One <43485962+c-git@users.noreply.github.com>
2023-11-23 01:56:31 -0500

    Sub 2

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -3,7 +3,7 @@ pub fn is_armstrong_number(num: u32) -> bool {
         return true;
     }
     let mut remainder = num;
-    let num_digits = (num as f64).log10().floor() as u32 + 1;
+    let num_digits = num.ilog10() + 1;
     let mut sum = 0;
     while remainder > 0 && sum <= num {
         let last_digit = remainder % 10;
