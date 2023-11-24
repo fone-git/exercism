5456f01a1e08def3d94887128155f446954f8b1a
One <43485962+c-git@users.noreply.github.com>
2023-11-24 18:27:38 -0500

    Sub 1

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -2,17 +2,22 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: f64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+        Self { seconds: s as _ }
     }
 }
 
 pub trait Planet {
+    const EARTH_DAYS_PER_YEAR: f64 = 365.25;
+    const EARTH_SECONDS_PER_YEAR: f64 = Self::EARTH_DAYS_PER_YEAR * 24.0 * 60.0 * 60.0; // Days per year * Hours Per Day * Minutes Per Hour * Seconds Per Minute
+    fn earth_years_ratio() -> f64;
     fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+        d.seconds / Self::EARTH_SECONDS_PER_YEAR / Self::earth_years_ratio()
     }
 }
 
@@ -25,11 +30,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn earth_years_ratio() -> f64 {
+        0.2408467
+    }
+}
+impl Planet for Venus {
+    fn earth_years_ratio() -> f64 {
+        0.61519726
+    }
+}
+impl Planet for Earth {
+    fn earth_years_ratio() -> f64 {
+        1.0
+    }
+}
+impl Planet for Mars {
+    fn earth_years_ratio() -> f64 {
+        1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn earth_years_ratio() -> f64 {
+        11.862615
+    }
+}
+impl Planet for Saturn {
+    fn earth_years_ratio() -> f64 {
+        29.447498
+    }
+}
+impl Planet for Uranus {
+    fn earth_years_ratio() -> f64 {
+        84.016846
+    }
+}
+impl Planet for Neptune {
+    fn earth_years_ratio() -> f64 {
+        164.79132
+    }
+}
