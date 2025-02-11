756095879f042aa65741705c2ecaa018df10f68a
One <43485962+c-git@users.noreply.github.com>
2025-02-10 22:51:44 -0500

    csharp[submit]: lucians-luscious-lasagna

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,22 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
 
-    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int actual)
+    {
+        return this.ExpectedMinutesInOven() - actual;
+    }
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layerCount)
+    {
+        return layerCount * 2;
+    }
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int ovenTime)
+    {
+        return this.PreparationTimeInMinutes(layers) + ovenTime;
+    }
 }
